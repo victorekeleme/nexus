4738d685285a573807c4fc7ea09a97f1cae1ce8d
__Vistein__ <74677661+victorekeleme@users.noreply.github.com>
2022-12-12 09:48:10 -0800

    Update nexusSetup.sh

--- nexusSetup.sh
+++ nexusSetup.sh
@@ -9,20 +9,24 @@ sudo hostnamectl set-hostname nexus
 #intalling neccessary packages
 sudo yum install wget git vim unzip -y
 
-sudo mkdir /opt/nexus_home
+#add system user for nexus
+sudo useradd -m -U -d /opt/nexus_home/ -s /bin/bash nexus
+
+#Grant sudo access to nexus user
+sudo echo "nexus ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/nexus
 
 #change directory
-cd /opt/nexus_home/
+cd /opt/nexus_home
 
 #checking for java
 java -version
 
 if [ $? -eq 0 ]
 then
-	echo "Java already installed"
+        echo "Java already installed"
 else
-	echo "Installing Java"
-	sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
+        echo "Installing Java"
+        sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
 fi
 
 #downloading, extracting nexus package
@@ -33,21 +37,15 @@ sudo tar -zxvf nexus-3.15.2-01-unix.tar.gz
 #cleaning nexus zippped package
 sudo rm -rf nexus-3.15.2-01-unix.tar.gz
 
-sudo mv /opt/nexus_home/nexus-* /opt/nexus_home/nexus
-
-#add system user for nexus
-sudo useradd -m -U -d /opt/nexus_home/ -s /bin/bash nexus
-
-#Grant sudo access to nexus user
-sudo echo "nexus ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/nexus
-
 # change the owner and group permissions to /opt/nexus and /opt/sonatype-work directories.
 sudo chown -R nexus:nexus /opt/nexus_home
 
 sudo chmod -R 775 /opt/nexus_home
 
+sudo mv /opt/nexus_home/nexus-3.15.2-01 /opt/nexus_home/nexus
+
 #update nexus run_as_user=""
-echo  'run_as_user="nexus" ' > /opt/nexus_home/nexus/bin/nexus.rc
+sudo echo  'run_as_user="nexus"' > /opt/nexus_home/nexus/bin/nexus.rc
 
 #configuring nexus to run as a service
 sudo ln -s /opt/nexus_home/nexus/bin/nexus /etc/init.d/nexus
